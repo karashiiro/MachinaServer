398579c6a95a92a38b6528238867d2715c15dcf3
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-10-21 16:09:35 -0700

    Beginning KR support

--- MachinaWrapper/src/Util.cs
+++ MachinaWrapper/src/Util.cs
@@ -1,4 +1,7 @@
-﻿namespace MachinaWrapper.Common
+﻿using System;
+using System.IO;
+
+namespace MachinaWrapper.Common
 {
     static class Util
     {
@@ -8,6 +11,41 @@
         public static void JSify(ref string input)
         {
             input = input.Substring(0, 1).ToLower() + input.Substring(1);
+        }
+
+        /// <summary>
+        /// Return the Program Files (x86) location.
+        /// See https://stackoverflow.com/a/194223
+        /// </summary>
+        public static string ProgramFilesx86()
+        {
+            if (Environment.Is64BitOperatingSystem)
+            {
+                return Environment.GetEnvironmentVariable("ProgramFiles(x86)") ?? @"C:\Program Files (x86)";
+            }
+
+            return Environment.GetEnvironmentVariable("ProgramFiles") ?? @"C:\Program Files";
+        }
+
+        /// <summary>
+        /// Returns true if a Korean client can be found.
+        /// </summary>
+        public static bool SystemHasKRClient()
+        {
+            string[] folders = {
+                @"SquareEnix\FINAL FANTASY XIV - KOREA",
+                @"FINAL FANTASY XIV - KOREA"
+            };
+
+            foreach (string folder in folders)
+            {
+                if (Directory.Exists(Path.Combine(ProgramFilesx86(), folder)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
